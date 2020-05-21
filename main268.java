8da39856883889ed31418fc51c04720bfacddb2f
run615 <1020129891@qq.com>
2020-05-21 11:55:53 +0800

    缺失数字

---
+++ main268.java
@@ -0,0 +1,33 @@
+package Leetcode;
+
+
+/**
+ * 268. 缺失数字
+ * 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,0,1]
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: [9,6,4,2,3,5,7,0,1]
+ * 输出: 8
+ *
+ */
+public class main268 {
+    /**
+     * 利用异或，且满足结合律和交换律
+    */
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        // 先和新补的索引异或一下
+        res ^= n;
+        // 和其他的元素、索引做异或
+        for(int i = 0; i < n; i++){
+            res ^= i ^ nums[i];
+        }
+        return res;
+    }
+}
