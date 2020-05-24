6285d0022017a65127dc3368c7c58ed008df97ad
run615 <1020129891@qq.com>
2020-05-24 09:44:17 +0800

    寻找两个正序数组的中位数

---
+++ main4.java
@@ -0,0 +1,70 @@
+package Leetcode;
+
+
+/**
+ * 4. 寻找两个正序数组的中位数
+ * 中位数：指的是该数左右个数相等
+ */
+public class main4 {
+    /**
+     * 暴力法
+     * 将两个数组合并，然后根据奇数还是偶数，返回中位数
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums;
+        int m = nums1.length;
+        int n = nums2.length;
+        nums = new int[m + n];
+        if (m == 0) {
+            if (n % 2 == 0) {
+                return (nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
+            } else {
+
+                return nums2[n / 2];
+            }
+        }
+        if (n == 0) {
+            if (m % 2 == 0) {
+                return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
+            } else {
+                return nums1[m / 2];
+            }
+        }
+
+        int count = 0;
+        int i = 0, j = 0;
+        while (count != (m + n)) {
+            if (i == m) {
+                while (j != n) {
+                    nums[count++] = nums2[j++];
+                }
+                break;
+            }
+            if (j == n) {
+                while (i != m) {
+                    nums[count++] = nums1[i++];
+                }
+                break;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                nums[count++] = nums1[i++];
+            } else {
+                nums[count++] = nums2[j++];
+            }
+        }
+
+        if (count % 2 == 0) {
+            return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
+        } else {
+            return nums[count / 2];
+        }
+    }
+
+    public static void main(String[] args) {
+        main4 p = new main4();
+        double res = p.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4});
+        System.out.println(res);
+    }
+
+}
