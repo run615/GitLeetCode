34f7dd72a5a03dd4f3ee9f795a3950b65352ea31
run615 <1020129891@qq.com>
2020-05-26 14:32:23 +0800

    链表的中间节点

---
+++ main876.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+/**
+ * 876. 链表的中间结点
+ * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+ *
+ * 如果有两个中间结点，则返回第二个中间结点。
+ */
+public class main876 {
+    /**
+     * 快慢指针
+     */
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
